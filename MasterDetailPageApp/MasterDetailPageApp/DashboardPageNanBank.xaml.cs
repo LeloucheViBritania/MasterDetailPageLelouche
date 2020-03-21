7a755a446052fa8d74ebec0d370bded2b5e7b387
LeloucheViBriania <kra.kobenan.olivier@gmail.com>
2020-03-21 19:46:25 +0000

    folder

---
+++ MasterDetailPageApp/MasterDetailPageApp/DashboardPageNanBank.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MasterDetailPageApp
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class DashboardPageNanBank : ContentPage
+    {
+        public DashboardPageNanBank()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ MasterDetailPageApp/MasterDetailPageApp/MasterDetailPageUI.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MasterDetailPageApp
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MasterDetailPageUI : MasterDetailPage
+    {
+        public MasterDetailPageUI()
+        {
+            InitializeComponent();
+            Detail = new NavigationPage(new DashboardPageNanBank());
+            IsPresented = false;
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            Detail = new NavigationPage(new DashboardPageNanBank());
+            IsPresented = false;
+        }
+    }
+}
